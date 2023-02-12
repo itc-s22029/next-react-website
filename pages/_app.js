1c27d9d9347312013308b21e98d9c5cecd6e7a46
s22029 <s22029@std.it-college.ac.jp>
2023-02-12 14:02:09 +0000

    10th commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,51 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import 'styles/globals.css'
 import Layout from 'components/layout'
+import Script from 'next/script'
+import * as gtag from 'lib/gtag'
 
 // Font Awesome の設定
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false
+
 function MyApp ({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouterChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routerChangeComplete', handleRouterChange)
+    return () => {
+      router.events.off('routerChangeComplete', handleRouterChange)
+    }
+  }, [router.events])
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        Strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MASUREMENT_ID}`}
+      />
+      <Script
+        id='gtag-init'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+      `
+        }}
+      />
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
